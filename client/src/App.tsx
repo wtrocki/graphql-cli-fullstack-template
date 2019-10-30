9e25960a79b3572c7bd0b4cdefba7fb2e09b1f27
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-10-30 17:46:26 -0400

    Enhance frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 import { useCreateNoteMutation, useFindAllNotesQuery } from './generated-types';
-import logo from './logo.svg';
 
 const App: React.FC = () => {
   const allNotes = useFindAllNotesQuery();
@@ -10,35 +9,33 @@ const App: React.FC = () => {
   const [newNoteTitle, setNewNoteTitle] = useState();
   const [newNoteDescription, setNewNoteDescription] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <form onSubmit={() => createNote({ variables: { title: newNoteTitle, description: newNoteDescription }})}>
+    <div>
+      <fieldset>
         <legend>Create New Note</legend>
-        <label htmlFor="title">Title</label>
-        <input name="title" value={newNoteTitle} onChange={(e) => setNewNoteTitle(e.target.value)} />
-        <label htmlFor="description">Description</label>
-        <input name="description" value={newNoteDescription} onChange={(e) => setNewNoteDescription(e.target.value)} />
-        <input type='submit' />
-      </form>
+        <form onSubmit={() => createNote({ variables: { title: newNoteTitle, description: newNoteDescription } })}>
+          <p>
+            <label htmlFor="title">Title: </label>
+            <input
+              name="title"
+              value={newNoteTitle}
+              onChange={(e) => setNewNoteTitle(e.target.value)} />
+          </p>
+          <p>
+            <label htmlFor="description">Description: </label>
+            <input
+              name="description"
+              value={newNoteDescription}
+              onChange={(e) => setNewNoteDescription(e.target.value)} />
+          </p>
+          <input type='submit' />
+        </form>
+      </fieldset>
       <ul>
-      {
-        allNotes.data && allNotes.data.findAllNotes.map((note) => (
-        <li><strong>{note.title}</strong>{note.description}</li>
-        ))
-      }
+        {
+          allNotes.data && allNotes.data.findAllNotes.map((note) => (
+            <li><strong>{note.title}</strong>{note.description}</li>
+          ))
+        }
       </ul>
     </div>
   );
